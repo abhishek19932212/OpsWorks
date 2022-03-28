048271e0af2b2a82d8733e2a2ef3ed3ac6b89b4b
abhishek19932212 <55378667+abhishek19932212@users.noreply.github.com>
2022-03-29 00:01:09 +0530

    Update default.rb

--- ssh_custome_key/recipes/default.rb
+++ ssh_custome_key/recipes/default.rb
@@ -1,1 +1,27 @@
-write_host_key(:path => '/etc/ssh/id_ecdsa', :content => node['ssh_host_ecdsa_key_private'], :mode => 0600 :owner => deploy :group => www-data)
+#
+# Cookbook Name:: copperegg_custom
+# Recipe:: default
+#
+
+if platform?('redhat', 'centos', 'fedora', 'ubuntu', 'debian', 'amazon')
+ 
+  template '/tmp/config_change.sh' do
+    owner 'root'
+    group 'root'
+    source 'config_change.sh.erb'
+    mode 0554
+    action :create
+  end
+
+  script 'config_change' do
+    interpreter 'bash'
+    cwd
+    user 'root'
+    code <<-EOH
+        /tmp/config_change.sh #{node['copperegg_custom']['apikey']} #{node['copperegg_custom']['www']} >> /tmp/config_change.log 2>&1
+    EOH
+    action :run
+    only_if { !::File.exists?("/home/deploy/.ssh/config") } 
+  end
+
+end
