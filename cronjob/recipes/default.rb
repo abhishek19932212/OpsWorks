c07c31c896456615614b05d26ed5f15e93b71ca5
abhishek19932212 <55378667+abhishek19932212@users.noreply.github.com>
2022-03-31 15:58:35 +0530

    Update default.rb

--- cronjob/recipes/default.rb
+++ cronjob/recipes/default.rb
@@ -1,6 +1,12 @@
-bash "restart_monit" do
+bash "reconfigure config file" do
     user "root"
     code <<-EOH
-    monit restart all
+    if [ -f "/home/deploy/.ssh/config" ] ; then
+    rm "$file"
+    fi
+    touch /home/deploy/.ssh/config
+    echo "Host github.com\nIdentityFile /home/deploy/.ssh/id_ecdsa\nStrictHostKeyChecking no" > /home/deploy/.ssh/config
+    chmod 600 /home/deploy/.ssh/config
+    chown deploy:www-data /home/deploy/.ssh/config
     EOH
 end
