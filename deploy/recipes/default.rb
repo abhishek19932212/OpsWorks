8d3de6757640ca24727f0ef69cbf234c320f045d
abhishek19932212 <55378667+abhishek19932212@users.noreply.github.com>
2022-04-04 22:29:32 +0530

    Update default.rb

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -1,5 +1,14 @@
 Chef::Log.level = :debug
 
+bash "Success Running Bash" do
+  user "root"
+  cwd current_release
+  code <<-EOH
+    echo "Success Running Bash"
+  EOH
+
+end
+
 include_recipe 'dependencies'
 
 node[:deploy].each do |application, deploy|
@@ -7,14 +16,5 @@ node[:deploy].each do |application, deploy|
   opsworks_deploy_user do
     deploy_data deploy
   end
-
-end
-
-bash "stop resque workers" do
-  user "root"
-  cwd current_release
-  code <<-EOH
-    echo "Success Running Bash"
-  EOH
 
 end
